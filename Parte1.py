9e269bc87394e0ddb1dd7a22e7d05496f7a2f034
Brandon <brandon.monsalve@sansano.usm.cl>
2023-10-10 18:36:32 -0300

    m

---
+++ Parte1.py
@@ -0,0 +1,24 @@
+# Abre el archivo .txt
+f = open("input.txt", "r")
+
+while True:
+    n = f.readline().strip()
+
+    # Asegurarse que no sea linea vacia
+    if not n:
+      break
+    n = int(n)
+    if n == 0:
+        break  
+    print(n)
+
+    # Creamos un arreglo de las cajas
+    cajas = []
+
+    # Leemos N lineas
+    for _ in range(n):
+        # Metemos las dimensiones de la caja i en el arreglo
+        dimensiones = list(map(int, f.readline().strip().split()))
+        cajas.append(dimensiones)
+    for i in cajas:
+      print(i)
